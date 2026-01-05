cc39661d82574a777aa31809245b12bb43b64371
Himanshu Singh Negi <singhnegi11@users.noreply.github.com>
2026-01-05 23:04:18 +0530

    Update query.sql

--- day01/query.sql
+++ day01/query.sql
@@ -9,3 +9,11 @@ with cte as (select *,datename(weekday,order_date) as day,month(order_date) as m
 select category,sum(amount) from cte
 where day not in ('Saturday','Sunday') and month = 2
 group by category;
+
+---query 3
+with cte as (select category_id,sum(amount) as total_sales from sales 
+group by category_id)
+
+select category_name,COALESCE(ct.total_sales,0) from categories cg
+left join cte ct on cg.category_id = ct.category_id
+order by ct.total_sales
